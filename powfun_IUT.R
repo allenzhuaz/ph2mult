83690f7547405c4fd0141b821bb530ddce5adf1b
Yalin (Allen) <allenzhuaz@users.noreply.github.com>
2016-06-21 17:13:01 -0400

    Add the output option of PET and EN
    Add the output option of PET and EN

--- powfun_IUT.R
+++ powfun_IUT.R
@@ -4,10 +4,9 @@
 # "s2.f" indicates two-stage design with early stop for only futility.
 design.methods <- c("s1","s2.sf","s2.f")
 
-# Power function for Intersection (rejection region) - Union (accept region/null hypothesis) Test
-IUT.power <- function(method=design.methods, s1.rej, s1.acc, t1.rej, t1.acc, s2.rej, t2.rej, n1, n2, n, p.s, p.t, output.all = FALSE){
+UIT.power <- function(method=design.methods,s1.rej, s1.acc, t1.rej, t1.acc, s2.rej, t2.rej, 
+                      n1, n2, n, p.s, p.t, output.all = FALSE){
   
-  # to avoid the logrithm bug for 'dbinom' function, we re-define the binomial density function 
   binom.dens <- function(x, size, prob){choose(size,x)*prob^x*(1-prob)^(size-x)}  
   
   s1.pow <- function(s2.rej, t2.rej, n, p.s, p.t){
@@ -17,21 +16,21 @@ IUT.power <- function(method=design.methods, s1.rej, s1.acc, t1.rej, t1.acc, s2.
     # p.s, p.t : probability of pCR and ePD
     
     pmf <- 0
-    for (t in 0:min(n-s2.rej,t2.rej)){
-      for (s in s2.rej:(n-t)){
+    for (t in (t2.rej+1):n){
+      for (s in 0:min(s2.rej, n-t)){
         pmf <- pmf + binom.dens(s, size = n-t, prob = p.s/(1-p.t))*binom.dens(t,size = n, prob = p.t)
       }
     }
-    return(pmf)
+    return(1-pmf)
   }
   method <- match.arg(method)
-  Pow <- switch(method, s1 = {
+  Pow <- switch( method, s1 = {
     return( s1.pow(s2.rej, t2.rej, n, p.s, p.t) )
   }, s2.sf= {
     ## this block uses the following parameters:s1.rej, t1.rej, s1.acc, t1.acc, s2.rej, t2.rej, n1, n2, p.s, p.t
-    
-    # s1.rej, t1.rej : rejection boundary of H0 at the first stage -- s >= s1.rej AND t <= t1.rej
-    # s1.acc, t1.acc : acceptance boundary of H0 at the first stage -- s <= s1.acc OR t >= t1.acc
+    # H0: p1 <= p01 and p2 >= p02
+    # s1.rej, t1.rej : rejection boundary of H0 at the first stage -- s >= s1.rej OR t <= t1.rej
+    # s1.acc, t1.acc : acceptance boundary of H0 at the first stage -- s <= s1.acc AND t >= t1.acc
     # s2.rej, t2.rej : reject boundary of H0 at the second stage
     # n1, n2 : sample sizes of the first and the second stages
     # p.s, p.t: probability of pCR and ePD
@@ -40,80 +39,110 @@ IUT.power <- function(method=design.methods, s1.rej, s1.acc, t1.rej, t1.acc, s2.
     ## reject H0 at the first stage
     pmf <- s1.pow(s1.rej, t1.rej, n1, p.s, p.t)
     
+    ## set initial probability of continue to the second stage as 0
+    PCon <- 0
+    
     ## continue after the first stage and then reject H0 at the second stage
     
     # first continuation region: s2 < si < s1 and 0 <= ti < n1-si
-    if ((t1.rej > n1-s1.rej)&(t1.acc > n1-s1.acc)){
-      for ( si in (s1.acc+1):(s1.rej-1) ) {
-        for (ti in 0:(n1-si) ) {
+    if ((t1.acc > n1-s1.acc) & (t1.rej > n1-s1.rej)){
+      for ( ti in (t1.rej+1):(t1.acc-1) ) {
+        for (si in 0:(n1-ti) ) {
           # replace the infinite single stage power by max limit 1
           p <- s1.pow(s2.rej-si, t2.rej-ti, n2, p.s, p.t)
           p[is.nan(p)]=1
-          pmf <- pmf + binom.dens(ti,size = n1-si,prob = p.t/(1-p.s)) * binom.dens(si,size = n1, prob = p.s) * p
+          pmf <- pmf + binom.dens(ti,size = n1-ti,prob = p.s/(1-p.t)) * binom.dens(ti,size = n1, prob = p.t) * p
+          
+          PCon <- Pcon + binom.dens(ti,size = n1-ti,prob = p.s/(1-p.t)) * binom.dens(ti,size = n1, prob = p.t)
         }
       }
     }
     
+    
+    
     # second continuation region: ( s2 < si < s1 and 0 <= ti <= t1 ) and ( s2 < si < n1-ti and t1 < ti <= n1-s2 )
-    #else if ((t1.rej <= n1-s1.rej) & (t1.acc > n1-s1.acc)){
     else {
-      for (ti in 0:min(t1.rej,n1-s1.rej)) {
-        for ( si in (s1.acc+1):(s1.rej-1) ) {
+      for (si in 0:min(s1.acc,n1-t1.acc)){
+        for ( ti in (t1.rej+1):(t1.rej-1) ) {
           p <- s1.pow(s2.rej-si, t2.rej-ti, n2, p.s, p.t)
           p[is.nan(p)]=1
-          pmf <- pmf + binom.dens(si,size = n1-ti,prob = p.s/(1-p.t))*binom.dens(ti,size = n1, prob = p.t) * p
+          pmf <- pmf + binom.dens(ti,size = n1-si,prob = p.t/(1-p.s)) * binom.dens(si,size = n1, prob = p.s) * p
+          
+          PCon <- Pcon + binom.dens(ti,size = n1-si,prob = p.t/(1-p.s)) * binom.dens(si,size = n1, prob = p.s)
         }  
       }
-      for (ti in (min(t1.rej,n1-s1.rej)+1):min(n1-s1.acc,t1.acc-1)){
-        for (si in (s1.acc+1):(n1-ti)){
+      for (si in (min(s1.acc,n1-t1.acc)+1):min((n1-t1.rej),(s1.rej-1))){
+        for (ti in (t1.rej+1):(n1-si)){
           p <- s1.pow(s2.rej-si, t2.rej-ti, n2, p.s, p.t)
           p[is.nan(p)]=1
-          pmf <- pmf + binom.dens(si,size = n1-ti,prob = p.s/(1-p.t))*binom.dens(ti,size = n1, prob = p.t) * p
+          pmf <- pmf + binom.dens(ti,size = n1-si,prob = p.t/(1-p.s)) * binom.dens(si,size = n1, prob = p.s) * p
+          
+          PCon <- PCon + binom.dens(ti,size = n1-si,prob = p.t/(1-p.s)) * binom.dens(si,size = n1, prob = p.s) 
+          
         }
-      }
+      } 
     } 
     
-    # third continuation region: ( s2 < si < s1 and 0 <= ti <= t1 ) and ( s2 < si < n1-ti and t2 < ti <= n1-s2 )
-    # else if ((t1.rej <= n1-s1.rej) & (t1.acc <= n1-s1.acc)){
-    #   for (ti in 0:t1.rej) {
-    #     for ( si in (s1.acc+1):(s1.rej-1) ) {
-    #       p <- s1.pow(s2.rej-si, t2.rej-ti, n2, p.s, p.t)
-    #       p[is.nan(p)]=1
-    #       pmf <- pmf + binom.dens(si,size = n1-ti,prob = p.s/(1-p.t))*binom.dens(ti,size = n1, prob = p.t) * p
-    #     }  
-    #   }
-    #   for (ti in (t1.rej+1):(t1.acc-1)){
-    #     for (si in (s1.acc+1):(n1-ti)){
-    #       p <- s1.pow(s2.rej-si, t2.rej-ti, n2, p.s, p.t)
-    #       p[is.nan(p)]=1
-    #       pmf <- pmf + binom.dens(si,size = n1-ti,prob = p.s/(1-p.t))*binom.dens(ti,size = n1, prob = p.t) * p
-    #     }
-    #   }
-    # }
-    return(pmf)
+    if (output.all == FALSE){
+      return(pmf)
+    }   
+    
+    else if (output.all == TRUE){ 
+      PET <- 1-PCon
+      EN <- n1*PET+(n1+n2)*(1-PET)
+      return(c(pmf,PET,EN))
+    }
   }, s2.f = {  
     ## this block use the following parameters: s1.acc, t1.acc, s2.rej, t2.rej, n1, n2, p.s, p.t
     # s1.acc, t1.acc : acceptance boundary of H0 at the first stage -- s <= s1.acc OR t >= t1.acc
     # s2.rej, t2.rej : reject boundary of H0 at the second stage
     # n1, n2 : sample sizes of the first and the second stages
     # p.s, p.t: probability of pCR and ePD
+    # for UIT, the continuous region is s2 < s <= N1 or 0 <= t < t2 
     
     ## Set initial power as 0
     pmf <- 0
+    
+    ## set initial continue probability as 0
     PCon <- 0
+    
     ## continue after the first stage and then reject H0 at the second stage
     
-    for (ti in 0:min(t1.acc-1,n1-s1.acc)) {
-      for ( si in (s1.acc+1):(n1-ti) ) {
-        p <- s1.pow(s2.rej-si, t2.rej-ti, n2, p.s, p.t)
-        p[is.nan(p)]=1
-        
-        PCon <- PCon + binom.dens(si,size = n1-ti,prob = p.s/(1-p.t))*binom.dens(ti,size = n1, prob = p.t) 
-        
-        
-        pmf <- pmf + binom.dens(si,size = n1-ti,prob = p.s/(1-p.t))*binom.dens(ti,size = n1, prob = p.t) * p
-      }  
+    # first continuation region: s2 < si <= n1 and 0 <= ti < n1-si
+    if (t1.acc <= n1-s1.acc){
+      for (si in 0:s1.acc) {
+        for (ti in 0:(t1.acc-1)) {
+          # replace the infinite single stage power by max limit 1
+          p <- s1.pow(s2.rej-si, t2.rej-ti, n2, p.s, p.t)
+          p[is.nan(p)]=1
+          pmf <- pmf + binom.dens(ti,size = n1-si,prob = p.t/(1-p.s))*binom.dens(si,size = n1, prob = p.s) * p
+          
+          PCon <- PCon + binom.dens(ti,size = n1-si,prob = p.t/(1-p.s))*binom.dens(si,size = n1, prob = p.s)
+        }
+      }
+      for (si in (s1.acc+1):n1 ) {
+        for (ti in 0:(n1-si)) {
+          # replace the infinite single stage power by max limit 1
+          p <- s1.pow(s2.rej-si, t2.rej-ti, n2, p.s, p.t)
+          p[is.nan(p)]=1
+          pmf <- pmf + binom.dens(ti,size = n1-si,prob = p.t/(1-p.s))*binom.dens(si,size = n1, prob = p.s) * p
+        }
+      }
     }
+    
+    # second continuation region: 0 <= ti < t2 and  s2 < si < n1-ti 
+    else if (t1.acc > n1-s1.acc){
+      for (ti in 0:(t1.acc-1)) {
+        for ( si in 0:(n1-ti) ) {
+          p <- s1.pow(s2.rej-si, t2.rej-ti, n2, p.s, p.t)
+          p[is.nan(p)]=1
+          pmf <- pmf + binom.dens(si,size = n1-ti,prob = p.s/(1-p.t))*binom.dens(ti,size = n1, prob = p.t) * p
+          
+          PCon <- PCon + binom.dens(si,size = n1-ti,prob = p.s/(1-p.t))*binom.dens(ti,size = n1, prob = p.t)
+        }  
+      }
+    } 
+    
     if (output.all == FALSE){
       return(pmf)
     }   
